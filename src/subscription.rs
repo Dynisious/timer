dab5925d29df4cd584af6d9e0ab7687832f1fb4a
Dynisious <daniel.bechaz@gmail.com>
2018-05-07 01:35:07 +1000

    Added Iteration
    `TimerSub` can now be iterated over.

--- src/subscription.rs
+++ src/subscription.rs
@@ -1,5 +1,6 @@
 
 use std::ops::Deref;
+use std::iter::Iterator;
 use super::{spawn, Sender, Receiver, TimerSignal,};
 pub use super::{mpsc::RecvError, thread::JoinHandle,};
 
@@ -27,12 +28,12 @@ pub struct TimerSub {
 impl TimerSub {
     pub fn new(subscription: Receiver<TimerSignal>, id: u32) -> Self { Self { subscription, id } }
     pub fn get_id(&self) -> u32 { self.id }
-    pub fn signal(&self) -> Result<TimerSignal, RecvError> { self.subscription.recv() }
+    pub fn signal(&self) -> Option<TimerSignal> { self.subscription.recv().ok() }
     pub fn run<F, P>(self, mut run: F) -> JoinHandle<()>
         where F: 'static + FnMut(P) + Send + Sync,
             P: From<TimerSignal>, {
         spawn(move || while {
-            if let Ok(signal) = self.signal() {
+            if let Some(signal) = self.signal() {
                 run(signal.into());
 
                 match signal {
@@ -44,4 +45,10 @@ impl TimerSub {
             } else { false }
         } {})
     }
+}
+
+impl Iterator for TimerSub {
+    type Item = TimerSignal;
+
+    fn next(&mut self) -> Option<Self::Item> { self.signal() }
 }
